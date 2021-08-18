a8b14fa0536d03ab4e4d071972f4f20789c6b56c
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-08-18 10:35:34 +0200

    Export functions from submodules.

--- src/BetheNetworks.jl
+++ src/BetheNetworks.jl
@@ -6,4 +6,15 @@ include("SpectralParameters.jl")
 include("BetheEnergy.jl")
 include("BetheNetwork.jl")
 
+using .Pauli
+using .LaxOperators
+
+using .SpectralParameters
+using .BetheEnergy
+using .BetheNetwork
+
+export Spin, pauli, lax_operator, lax_operators
+export solveBAE, solve_groundstate_BAE
+export bethe_energy, heisenberg_hamiltonian_MPO, bethe_network, bethe_MPS
+
 end
