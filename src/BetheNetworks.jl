ad53da94bd2ecc897fcdaed755544f57cd4e15d1
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-09-07 17:07:53 +0200

    Implement simple main function.

--- src/BetheNetworks.jl
+++ src/BetheNetworks.jl
@@ -17,4 +17,21 @@ export Spin, pauli, lax_operator, lax_operators
 export solveBAE, solve_groundstate_BAE
 export bethe_energy, heisenberg_hamiltonian_MPO, bethe_network, bethe_MPS
 
+using BlockTensors
+using BlockTensors.MatrixProductStates
+
+function main(N, maxdim_initial, maxdim)
+    p = Space("p")
+    a = Space("a")
+    spectrals = solve_groundstate_BAE(N)
+    network = bethe_network(N, spectrals, p, a)
+    MPS = bethe_MPS(network, a, maxdim_initial)
+    norms = optimize_betheMPS!(MPS, network, p, a, maxdim)
+    
+    energy = bethe_energy(N, spectrals)
+    hamiltonMPO = heisenberg_hamiltonian_MPO(N, p, Space("state"))
+    deviation = abs(real(expectationvalue(hamiltonMPO, MPS) - energy) / energy)
+    return deviation, MPS, norms
+end
+
 end
