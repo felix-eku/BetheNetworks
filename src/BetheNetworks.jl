391174f0aa1f4b6d17d0dd7daeb823d2a1d3bdb8
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-09-22 22:04:58 +0200

    Implement entanglement_data.
    Compute bond dimensions and entropies of a MPS.

--- src/BetheNetworks.jl
+++ src/BetheNetworks.jl
@@ -20,6 +20,8 @@ export bethe_energy, heisenberg_hamiltonian_MPO, bethe_network, bethe_MPS
 using BlockTensors
 using BlockTensors.MatrixProductStates
 
+using TypedTables
+
 function main(N, maxdim_initial, maxdim)
     p = Space("p")
     a = Space("a")
@@ -35,6 +37,16 @@ function main(N, maxdim_initial, maxdim)
     deviation = abs(real(expectationvalue(hamiltonMPO, MPS) - energy) / energy)
     
     return original_deviation, deviation, MPS, contractions, norms
+end
+
+function entanglement_data(MPS, connecting)
+    bonds = 1 : length(MPS) - 1
+    dimensions = [
+        only(matching(connecting, MPS[eachindex(MPS)[bond]])).dimensions.totaldim
+        for bond in bonds
+    ]
+    entropies = [entanglement_entropy(MPS, bond, connecting) for bond in bonds]
+    return Table(bond = bonds, dimension = dimensions, entropy = entropies)
 end
 
 end
