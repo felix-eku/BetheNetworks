df92a3b3c9c999b3ac034a7e13fbc953928bb9bc
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-11-29 19:11:29 +0100

    Improve implementation of lax_operator.
    Add tag to the auxiliary space by default.
    Also simplify lax_operators.

--- src/LaxOperators.jl
+++ src/LaxOperators.jl
@@ -6,29 +6,32 @@ using ..Pauli
 
 export lax_operator, lax_operators
 
-function lax_operator(u, physical, auxiliary, types...)
-    scaling = u * pauli(0, physical, types...) * pauli(0, auxiliary, types...)
+function lax_operator(
+    u, physical::Union{Space, Connector}, auxiliary::Union{Space, Connector}, types...
+)
+    phys = copy(physical)
+    aux = copy(auxiliary)
+    addtags!(aux; u = round(u, digits = 3))
+    lax_operator(u, Outgoing(phys), Incoming(phys), Outgoing(aux), Incoming(aux), types...)
+end
+
+function lax_operator(
+    u, physout::Union{Leg{S, Outgoing}, Outgoing}, physin::Union{Leg{S, Incoming}, Incoming},
+    auxout::Union{Leg{S, Outgoing}, Outgoing}, auxin::Union{Leg{S, Incoming}, Incoming}, 
+    types...
+) where S <: SymmetrySector
+    scaling = u * pauli(0, physout, physin, types...) * pauli(0, auxout, auxin, types...)
     return scaling + (im/2) * sum(1:3) do index
-        pauli(index, physical, types...) * pauli(index, auxiliary, types...)
+        pauli(index, physout, physin, types...) * pauli(index, auxout, auxin, types...)
     end
 end
 
 function lax_operators(spectral_parameters, physical, auxiliary, types...)
     operators = [
-        (
-            p = copy(physical); a = copy(auxiliary); 
-            addtags!(a; u = round(u, digits = 3)); 
-            lax_operator(u, p, a, types...)
-        )
+        lax_operator(u, physical, auxiliary, types...)
         for u in spectral_parameters
     ]
-    K = LinearIndices(operators)
-    for (k1, k2) in zip(K[begin : end - 1], K[begin + 1 : end])
-        connect!(
-            only(matching(Outgoing(physical), operators[k1])),
-            only(matching(Incoming(physical), operators[k2])),
-        )
-    end
+    connectchain!(operators, physical)
     return operators
 end
 
