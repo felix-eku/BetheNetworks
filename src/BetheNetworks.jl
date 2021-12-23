9de7286d0bab92f92534c0822ce6e3feca9579ec
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-12-23 11:30:51 +0100

    Implement main functions for computing deviations.

--- src/BetheNetworks.jl
+++ src/BetheNetworks.jl
@@ -21,7 +21,7 @@ export study_betheMPS
 using BlockTensors
 using BlockTensors.MatrixProductStates
 
-using TypedTables
+using TypedTables, CSV
 
 function study_betheMPS(
     N, Bₙ, truncation, truncation_transverse, ::Type{S} = Spin;
@@ -75,4 +75,71 @@ function truncation_comparison(N, Bₙ, maxrelerror)
     deviation_maxdim, = study_betheMPS(N, Bₙ, (; maxdim = bond_dimension), ())
     return bond_dimension, deviation_relerror, deviation_maxdim
 end
+
+function main_original_no_blocks()
+    Ns = 16:2:36
+    deviations = Array{Float64}(undef, length(Ns), 6)
+    bond_dims = similar(deviations, Int)
+    for (i, N) in enumerate(Ns)
+        Bₙs = [groundstate_Bₙ(N), -N/2 + 2 : 2 : +N/2 - 2]
+        truncations = [(; maxdim = 20N), (; maxdim = 30N), (; maxrelerror = 1e-9)]
+        for (configuration, (truncation, Bₙ)) in enumerate(Iterators.product(truncations, Bₙs))
+            deviation, MPS = study_betheMPS(N, Bₙ, truncation, (), Trivial)
+            bond_dim = maximum(bond_dimension(MPS, bond, Outgoing(:a)) for bond = 1:N-1)
+            @show (N, deviation, bond_dim)
+            deviations[i, configuration] = deviation
+            bond_dims[i, configuration] = bond_dim
+        end
+    end
+    table = Table(
+        N = Ns,
+        deviation_ground_state_maxdim_20N = deviations[:, 1],
+        deviation_ground_state_maxdim_30N = deviations[:, 2],
+        deviation_ground_state_truncation_error_e9 = deviations[:, 3],
+        deviation_excited_state_maxdim_20N = deviations[:, 4],
+        deviation_excited_state_maxdim_30N = deviations[:, 5],
+        deviation_excited_state_truncation_error_e9 = deviations[:, 6],
+        bond_dim_ground_state_maxdim_20N = bond_dims[:, 1],
+        bond_dim_ground_state_maxdim_30N = bond_dims[:, 2],
+        bond_dim_ground_state_truncation_error_e9 = bond_dims[:, 3],
+        bond_dim_excited_state_maxdim_20N = bond_dims[:, 4],
+        bond_dim_excited_state_maxdim_30N = bond_dims[:, 5],
+        bond_dim_excited_state_truncation_error_e9 = bond_dims[:, 6],
+    )
+    table |> CSV.write("data/deviations_original_no_blocks.table", delim = ' ')
+end
+
+function main_original()
+    Ns = 20:2:50
+    deviations = Array{Float64}(undef, length(Ns), 6)
+    bond_dims = similar(deviations, Int)
+    for (i, N) in enumerate(Ns)
+        Bₙs = [groundstate_Bₙ(N), -N/2 + 2 : 2 : +N/2 - 2]
+        truncations = [(; maxdim = 10N), (; maxdim = 20N), (; maxrelerror = 1e-9)]
+        for (configuration, (truncation, Bₙ)) in enumerate(Iterators.product(truncations, Bₙs))
+            deviation, MPS = study_betheMPS(N, Bₙ, truncation, ())
+            bond_dim = maximum(bond_dimension(MPS, bond, Outgoing(:a)) for bond = 1:N-1)
+            @show (N, deviation, bond_dim)
+            deviations[i, configuration] = deviation
+            bond_dims[i, configuration] = bond_dim
+        end
+    end
+    table = Table(
+        N = Ns,
+        deviation_ground_state_maxdim_10N = deviations[:, 1],
+        deviation_ground_state_maxdim_20N = deviations[:, 2],
+        deviation_ground_state_truncation_error_e9 = deviations[:, 3],
+        deviation_excited_state_maxdim_10N = deviations[:, 4],
+        deviation_excited_state_maxdim_20N = deviations[:, 5],
+        deviation_excited_state_truncation_error_e9 = deviations[:, 6],
+        bond_dim_ground_state_maxdim_10N = bond_dims[:, 1],
+        bond_dim_ground_state_maxdim_20N = bond_dims[:, 2],
+        bond_dim_ground_state_truncation_error_e9 = bond_dims[:, 3],
+        bond_dim_excited_state_maxdim_10N = bond_dims[:, 4],
+        bond_dim_excited_state_maxdim_20N = bond_dims[:, 5],
+        bond_dim_excited_state_truncation_error_e9 = bond_dims[:, 6],
+    )
+    table |> CSV.write("data/deviations_original.table", delim = ' ')
+end
+
 end
