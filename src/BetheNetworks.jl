1e85e758b0e65e67051eab27cf69878c2d756525
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-12-23 11:28:08 +0100

    Extend study_betheMPS.
    Implement use of symmetrized, real MPOs
    and extraction of entanglement entropy.

--- src/BetheNetworks.jl
+++ src/BetheNetworks.jl
@@ -15,38 +15,51 @@ using .BetheNetwork
 
 export Spin, pauli, lax_operator, lax_operators
 export solveBAE, groundstate_Bₙ
-export bethe_energy, heisenberg_hamiltonian_MPO, bethe_network, bethe_MPS
+export bethe_energy, heisenberg_hamiltonian_MPO, bethe_network, betheMPS
+export study_betheMPS
 
 using BlockTensors
 using BlockTensors.MatrixProductStates
 
 using TypedTables
 
-function study_BetheMPS(
+function study_betheMPS(
     N, Bₙ, truncation, truncation_transverse, ::Type{S} = Spin;
-    optimize = false
+    real_symmetric = false, optimize = false, bond = nothing
 ) where S <: SymmetrySector
     p = Space("p")
     a = Space("a")
     spectrals = solveBAE(N, Bₙ)
-    spectrals_optimalorder = similar(spectrals)
-    M = length(spectrals)
-    m = div(M, 2, RoundUp)
-    spectrals_optimalorder[1:2:M] = spectrals[m:-1:1]
-    spectrals_optimalorder[2:2:M] = spectrals[m+1:M]
-    network = bethe_network(N, spectrals_optimalorder, p, a, S)
-    MPS = bethe_MPS(network, a; truncation...)
+    m, r = divrem(length(spectrals), 2, RoundUp)
+    if real_symmetric
+        MPOs = symMPO.(N, spectrals[m + 1 : end], p, a, S)
+        if r < 0
+            @assert isapprox(spectrals[m], 0, atol = 4eps())
+            MPO0 = B_MPO(N, 0.0, p, a, S)
+            for n in LinearIndices(MPO0)
+                MPO0[n] *= (-1)^n * im
+            end
+            pushfirst!(MPOs, MPO0)
+        end
+        network = bethe_network(MPOs, p)
+    else
+        spectrals_optimalorder = similar(spectrals)
+        spectrals_optimalorder[1:2:end] = spectrals[m : -1 : begin]
+        spectrals_optimalorder[2:2:end] = spectrals[m + 1 : end]
+        network = bethe_network(N, spectrals_optimalorder, p, a, S)
+    end
+    MPS, entanglements = betheMPS(network, a, bond; truncation...)
 
-    energy = bethe_energy(N, spectrals_optimalorder)
+    energy = bethe_energy(N, spectrals)
     hamiltonMPO = heisenberg_hamiltonian_MPO(N, p, Space("state"), S)
-    original_deviation = abs(real(expectationvalue(hamiltonMPO, MPS) - energy) / energy)
+    original_deviation = abs(real(expectationvalue(hamiltonMPO, MPS, p) - energy) / energy)
     
-    optimize || return original_deviation, MPS
+    optimize || return original_deviation, MPS, entanglements
 
     norms, contractions = optimize_betheMPS!(MPS, network, p, a; truncation_transverse...) 
-    deviation = abs(real(expectationvalue(hamiltonMPO, MPS) - energy) / energy)
+    deviation = abs(real(expectationvalue(hamiltonMPO, MPS, p) - energy) / energy)
     
-    return original_deviation, deviation, MPS, contractions, norms
+    return original_deviation, deviation, MPS, entanglements, contractions, norms
 end
 
 function entanglement_data(MPS, connecting)
